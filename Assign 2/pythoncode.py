8806713064c55f4526a72c41200365a59c2616ee
AlinaFahim <92322865+AlinaFahim@users.noreply.github.com>
2022-05-09 07:36:03 +0500

    Update pythoncode.py

--- Assign 2/pythoncode.py
+++ Assign 2/pythoncode.py
@@ -3,8 +3,13 @@ import numpy as np
 import random
 
 testDF = panda.read_csv('test.csv')
+
 idDF = testDF[['id']]
+
 idDF.insert(1,"target",0.0)
+
 idDF['target'] = np.random.rand(700000,1)
+
 idDF.to_csv('sample.csv')
+
 print(idDF)
